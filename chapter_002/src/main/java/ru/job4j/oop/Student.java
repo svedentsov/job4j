19af1ddd1e9825f96696faa655ee2a35df791ccc
Igor Svedentsov <svedentsov@gmail.com>
2019-09-07 15:37:31 +0500

    1.3. Музыкальный автомат[#167353]

--- chapter_002/src/main/java/ru/job4j/oop/Student.java
+++ chapter_002/src/main/java/ru/job4j/oop/Student.java
@@ -1,5 +1,10 @@
 package ru.job4j.oop;
 
+/**
+ * @author Igor Svedentsov (svedensov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Student {
     public void music() {
         System.out.println("Tra tra tra");
