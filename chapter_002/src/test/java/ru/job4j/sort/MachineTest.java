5e6f19763737f268c304be57d2e7341c81bdbc8e
Igor Svedentsov <isvedentsov@artsofte.ru>
2020-02-15 18:05:18 +0500

    Сдача в кофе машине. [#34741]

---
+++ chapter_002/src/test/java/ru/job4j/sort/MachineTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.sort;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Проверка расчета сдачи в кофе-машине.
+ */
+public class MachineTest {
+
+    /**
+     * Сдачи нет, если цена равна внесенной сумме.
+     */
+    @Test
+    public void whenEquals() {
+        Machine machine = new Machine();
+        int[] expected = {};
+        int[] rsl = machine.change(100, 100);
+        assertThat(rsl, is(expected));
+    }
+
+    /**
+     * Сдачи нет, если цена больше внесенной суммы.
+     */
+    @Test
+    public void whenPriceMore() {
+        Machine machine = new Machine();
+        int[] expected = {};
+        int[] rsl = machine.change(50, 100);
+        assertThat(rsl, is(expected));
+    }
+
+    /**
+     * Сдача 15.
+     */
+    @Test
+    public void when50by35() {
+        Machine machine = new Machine();
+        int[] expected = {10, 5};
+        int[] rsl = machine.change(50, 35);
+        assertThat(rsl, is(expected));
+    }
+
+    /**
+     * Сдача 18.
+     */
+    @Test
+    public void when50by32() {
+        Machine machine = new Machine();
+        int[] expected = {10, 5, 2, 1};
+        int[] rsl = machine.change(50, 32);
+        assertThat(rsl, is(expected));
+    }
+}
