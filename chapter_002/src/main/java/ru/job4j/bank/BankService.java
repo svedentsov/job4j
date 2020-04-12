84099e5375a67dd1f5209ebe116bdd2334d9636b
Igor Svedentsov <svedentsov@gmail.com>
2020-04-12 14:14:30 +0500

    Банковские переводы.2

--- chapter_002/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_002/src/main/java/ru/job4j/bank/BankService.java
@@ -24,9 +24,7 @@ public class BankService {
      * @param user пользователь.
      */
     public void addUser(User user) {
-        if (!users.containsKey(user)) {
-            users.put(user, new ArrayList<>());
-        }
+        users.putIfAbsent(user, new ArrayList<>());
     }
 
     /**
