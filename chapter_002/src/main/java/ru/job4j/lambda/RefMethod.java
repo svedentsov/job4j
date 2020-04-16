50aa6a392af5f65418f54405415f6a2e9e8c362c
Igor Svedentsov <svedentsov@gmail.com>
2020-04-16 21:12:40 +0500

    0.1 Ссылки на методы.[#167183].

---
+++ chapter_002/src/main/java/ru/job4j/lambda/RefMethod.java
@@ -0,0 +1,32 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Замена лямбды ссылкой на метод.
+ */
+public class RefMethod {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList(
+                "Ivan",
+                "Petr Arsentev"
+        );
+        Consumer<String> out = RefMethod::cutOut;
+        names.forEach(out);
+    }
+
+    /**
+     * Обрезать строку больше 10 символов.
+     *
+     * @param value строка
+     */
+    public static void cutOut(String value) {
+        if (value.length() > 10) {
+            System.out.println(value.substring(0, 10) + "..");
+        } else {
+            System.out.println(value);
+        }
+    }
+}
