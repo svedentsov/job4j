f26ea0903a3bac216e216174a27beac321565add
Igor Svedentsov <svedentsov@gmail.com>
2020-04-26 12:25:05 +0500

    3. Массивы и модели. [#174892]

---
+++ chapter_001/src/main/java/ru/job4j/pojo/Product.java
@@ -0,0 +1,23 @@
+package ru.job4j.pojo;
+
+public class Product {
+    private String name;
+    private int count;
+
+    public Product(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
