168bc8ce1c04ce602829007d4295a3736cab6768
Igor Svedentsov <svedentsov@gmail.com>
2020-04-28 22:50:31 +0500

    0. Что такое исключение. [#3821]

---
+++ chapter_001/src/main/java/ru/job4j/ex/FrequentEx.java
@@ -0,0 +1,10 @@
+package ru.job4j.ex;
+
+public class FrequentEx {
+    public static void main(String[] args) {
+        String[] shops = {"Ebay", "Amazon", "Ozon"};
+        for (String shop : shops) {
+            System.out.println(shop);
+        }
+    }
+}
