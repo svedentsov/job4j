04de2b206b0bc701f1db1e61eb0857aa19398077
Игорь Сведенцов <svedentsov@gmail.com>
2020-09-02 22:56:25 +0500

    5.3.3.1. Очередь на двух стеках.

---
+++ 2_chapter_001/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -0,0 +1,35 @@
+package ru.job4j.list;
+
+public class SimpleQueue<T> {
+    private SimpleStack<T> inputStack;
+    private SimpleStack<T> outputStack;
+
+    public SimpleQueue() {
+        this.inputStack = new SimpleStack<>();
+        this.outputStack = new SimpleStack<>();
+    }
+
+    /**
+     * Удалить значение из коллекции.
+     * Если out-коллекция пустая, она заполняется значениями из in-коллекции до тех пор, пока in не будет пустая.
+     *
+     * @return возвращает значение и удаляет его из коллекции
+     */
+    public T poll() {
+        if (outputStack.isEmpty()) {
+            while (!inputStack.isEmpty()) {
+                outputStack.push(inputStack.poll());
+            }
+        }
+        return outputStack.poll();
+    }
+
+    /**
+     * Добавить значение в коллекцию.
+     *
+     * @param value добавляемое значение
+     */
+    public void push(T value) {
+        this.inputStack.push(value);
+    }
+}
