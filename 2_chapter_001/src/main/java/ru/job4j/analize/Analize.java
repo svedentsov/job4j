e0dda20d99541ec98609c67d3ee0ac76c68aa921
Игорь Сведенцов <svedentsov@gmail.com>
2020-09-12 21:10:08 +0500

    2. Статистика по коллекции.

---
+++ 2_chapter_001/src/main/java/ru/job4j/analize/Analize.java
@@ -0,0 +1,102 @@
+package ru.job4j.analize;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Класс реализует статистику об изменении коллекции.
+ */
+public class Analize {
+    /**
+     * Получить статистику об изменении коллекции.
+     *
+     * @param previous исходное состояние коллекции
+     * @param current  текущее состояние коллекции
+     * @return объект класса Info, содержащий статистику по коллекции
+     */
+    public Info diff(List<User> previous, List<User> current) {
+        Info info = new Info();
+        Map<Integer, String> usrMap = new HashMap<>();
+        for (User prevUser : previous) {
+            usrMap.put(prevUser.id, prevUser.name);
+        }
+
+        if (!usrMap.isEmpty() && current.size() == 0) {
+            info.deleted = previous.size();
+        }
+        for (User curUser : current) {
+            String s = usrMap.get(curUser.id);
+            if (s == null) {
+                info.added++;
+            }
+            if (s != null && !s.equals(curUser.name)) {
+                info.changed++;
+            }
+        }
+        info.deleted = previous.size() - current.size() + info.added;
+        return info;
+    }
+
+    /**
+     * Класс описывает отдельного пользователя.
+     */
+    public static class User {
+        /**
+         * Идентификатор пользователя.
+         */
+        int id;
+        /**
+         * Имя пользователя.
+         */
+        String name;
+
+        /**
+         * Конструктор.
+         *
+         * @param id   идентификатор пользователя
+         * @param name имя пользователя
+         */
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return id == user.id
+                    && Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+    }
+
+    /**
+     * Класс содержит статистику по коллекции.
+     */
+    public static class Info {
+        /**
+         * Число добавленных пользователей.
+         */
+        int added;
+        /**
+         * Число измененных пользователей.
+         */
+        int changed;
+        /**
+         * Число удаленных пользователей.
+         */
+        int deleted;
+    }
+}
---
+++ 2_chapter_001/src/test/java/ru/job4j/analize/AnalizeTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.analize;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Класс тестирует статистику об изменении коллекции.
+ */
+public class AnalizeTest {
+    Analize.User user1 = new Analize.User(1, "A");
+    Analize.User user2 = new Analize.User(2, "B");
+    Analize.User user2Changed = new Analize.User(2, "BB");
+    Analize.User user3 = new Analize.User(3, "C");
+
+    @Test
+    public void whenPreviousChanged() {
+        List<Analize.User> previous = Arrays.asList(user1, user2);
+        List<Analize.User> current = Arrays.asList(user1, user2Changed, user3);
+        Analize.Info info = new Analize().diff(previous, current);
+        assertThat(info.added, is(1));
+        assertThat(info.changed, is(1));
+        assertThat(info.deleted, is(0));
+    }
+
+    @Test
+    public void whenPreviousEmpty() {
+        List<Analize.User> previous = Arrays.asList();
+        List<Analize.User> current = Arrays.asList(user1, user2, user3);
+        Analize.Info info = new Analize().diff(previous, current);
+        assertThat(info.added, is(3));
+        assertThat(info.changed, is(0));
+        assertThat(info.deleted, is(0));
+    }
+
+    @Test
+    public void whenCurrentEmpty() {
+        List<Analize.User> previous = Arrays.asList(user1, user2, user3);
+        List<Analize.User> current = Arrays.asList();
+        Analize.Info info = new Analize().diff(previous, current);
+        assertThat(info.added, is(0));
+        assertThat(info.changed, is(0));
+        assertThat(info.deleted, is(3));
+    }
+}
