587dda2f1d5f9b798fd7dc24bd7c1fa64e061269
Игорь Сведенцов <svedentsov@gmail.com>
2020-09-18 23:15:57 +0500

    2. Анализ доступности сервера.

---
+++ 2_chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void whenServerUnavailable() throws IOException {
+        Analizy analizy = new Analizy();
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("500 13:56:01");
+            out.println("300 14:57:01");
+            out.println("400 15:58:01");
+            out.println("500 20:59:01");
+            out.println("200 22:01:02");
+            out.println("100 23:02:02");
+        }
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().map(el -> el + " ").forEach(rsl::append);
+        }
+        assertEquals(rsl.toString(), "13:56:01;14:57:01 15:58:01;22:01:02 ");
+    }
+}
