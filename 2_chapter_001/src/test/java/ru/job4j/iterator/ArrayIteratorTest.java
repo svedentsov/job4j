3437ed413d5161fe2c584babd55139eef293ac63
Игорь Сведенцов <svedentsov@gmail.com>
2021-02-19 23:51:40 +0300

    5.1.1. Итератор для двухмерного массива int[][]. Правки

--- 2_chapter_001/src/test/java/ru/job4j/iterator/ArrayIteratorTest.java
+++ 2_chapter_001/src/test/java/ru/job4j/iterator/ArrayIteratorTest.java
@@ -3,15 +3,17 @@ package ru.job4j.iterator;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class IteratorForArrayTest {
-    private IteratorForArray it;
+public class ArrayIteratorTest {
+    private ArrayIterator it;
 
     @Before
     public void setUp() {
-        it = new IteratorForArray(new int[][]{{1}, {3, 4}, {7}});
+        it = new ArrayIterator(new int[][]{{1}, {3, 4}, {6, 7}});
     }
 
     @Test
@@ -19,6 +21,7 @@ public class IteratorForArrayTest {
         assertThat(it.next(), is(1));
         assertThat(it.next(), is(3));
         assertThat(it.next(), is(4));
+        assertThat(it.next(), is(6));
         assertThat(it.next(), is(7));
     }
 
@@ -29,6 +32,7 @@ public class IteratorForArrayTest {
         assertThat(it.next(), is(1));
         assertThat(it.next(), is(3));
         assertThat(it.next(), is(4));
+        assertThat(it.next(), is(6));
         assertThat(it.next(), is(7));
     }
 
@@ -41,7 +45,35 @@ public class IteratorForArrayTest {
         assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(4));
         assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(7));
         assertThat(it.hasNext(), is(false));
+    }
+
+
+    @Test
+    public void whenThreeArrayWithoutNull() {
+        ArrayIterator it = new ArrayIterator(new int[][]{{1}, {2}, {3, 5}});
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(5));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testException() {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(6));
+        assertThat(it.next(), is(7));
+        it.next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testShouldThrowNoSuchElementException() {
+        it = new ArrayIterator(new int[][]{});
+        it.next();
     }
 }
