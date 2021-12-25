62246b2f470c59fdf842d87952b8aeac79e8e0c3
svedentsov <svedentsov@gmail.com>
2021-12-25 13:03:21 +0300

    3. Реализовать механизм программной остановки потока

--- 3_chapter_001/src/main/java/ru/job4j/pingpong/PingPong.java
+++ 3_chapter_001/src/main/java/ru/job4j/pingpong/PingPong.java
@@ -14,6 +14,13 @@ public class PingPong extends Application {
 
     private static final String JOB4J = "Пинг-понг www.job4j.ru";
 
+    /**
+     * Устанавливаем размеры игрового поля.
+     * Создаем группу.
+     * Создаем объект класса прямоугольник размером 10*10 и координатами его левого верхнего угла, добавляем его в группу.
+     * Создаем поток с переданным объектом RectangleMove, описывающим траекторию движения rect, и активируем его.
+     * Устанавливаем для сцены типа Stage заданные константные размеры, название и выводим на монитор.
+     */
     @Override
     public void start(Stage stage) {
         int limitX = 300;
@@ -21,10 +28,17 @@ public class PingPong extends Application {
         Group group = new Group();
         Rectangle rect = new Rectangle(50, 100, 10, 10);
         group.getChildren().add(rect);
-        new Thread(new RectangleMove(rect)).start();
+
+        Thread thread = new Thread(new RectangleMove(rect, limitX, limitY));
+        thread.start();
+
         stage.setScene(new Scene(group, limitX, limitY));
         stage.setTitle(JOB4J);
         stage.setResizable(false);
         stage.show();
+
+        stage.setOnCloseRequest(
+                event -> thread.interrupt()
+        );
     }
 }
