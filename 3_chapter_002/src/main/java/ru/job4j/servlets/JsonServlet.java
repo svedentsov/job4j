981720b25a132544373f76d315ad979600351996
svedentsov <svedentsov@gmail.com>
2022-01-27 21:17:49 +0300

    8.4. Ajax Jquery

---
+++ 3_chapter_002/src/main/java/ru/job4j/servlets/JsonServlet.java
@@ -0,0 +1,38 @@
+package ru.job4j.servlets;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class JsonServlet extends HttpServlet {
+
+    private PersonStorage storage = PersonStorage.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Person> persons = this.storage.findAll();
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(persons);
+
+        resp.setContentType("text/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter pw = new PrintWriter(resp.getOutputStream());
+        pw.append(json);
+        pw.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader br = req.getReader();
+        ObjectMapper mapper = new ObjectMapper();
+        Person person = mapper.readValue(br, Person.class);
+        this.storage.add(person);
+    }
+}
